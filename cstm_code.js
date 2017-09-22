4a307bac10699fccbf17b2854873f9e33bb29cca
soltesyzak <30795151+soltesyzak@users.noreply.github.com>
2017-09-22 14:16:58 +0200

    jazyky

--- cstm_code.js
+++
@@ -1,8 +0,0 @@
-// JavaScript Document
-$(".menu-anim").mouseenter(function(){
-	$(this).addClass("hop");
-});
-$(".menu-anim").mouseleave(function(){
-	$(this).removeClass("hop");
-});
-
---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+  <meta charset="UTF-8">
+  <meta name="description" content="Penzión Floriánov Dvor je novopostavený penzión a nachádza sa na okraji dediny Pleš, 15 km od mesta Lučenec.">
+  <meta name="keywords" content="Florianov dvor,Florianov-dvor,penzión,zabíjačka,Lučenec,Fiľakovo,wellness,poľovačka,gastronómia">
+  <meta name="author" content="Brickstone.design">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Florianov dvor</title>
+</head>
+
+<body>
+<?php
+	header('Location:./sk/index.php')
+?>
+</body>
+</html>
