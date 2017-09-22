d8cbac00bd971e07706f64bac8d4f97ae7ae3b1b
soltesyzak <30795151+soltesyzak@users.noreply.github.com>
2017-09-22 08:28:06 +0200

    de

---
+++ DE/new/cstm_code.js
@@ -0,0 +1,22 @@
+// JavaScript Document
+$( window ).load(function() {
+	setTimeout(function(){
+		$(".loading").fadeOut();
+	}, 1000);
+});
+
+$("button").click(function(){
+  $(".subdiv").toggleClass("active");
+});
+
+$(".menu-anim").mouseenter(function(){
+	$(this).addClass("hop");
+});
+$(".menu-anim").mouseleave(function(){
+	$(this).removeClass("hop");
+});
+
+$('.datepicker').datepicker({
+	format: 'dd/mm/yyyy',
+});
+
